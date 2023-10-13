2bf7c4cc214337c64463fc7be474b46f9524f219
Adam <mazurekdab65@gmail.com>
2023-10-13 18:00:35 +0200

    Adding logo svgs for the new header buttons

--- pws-frontend/src/components/header/Header.js
+++ pws-frontend/src/components/header/Header.js
@@ -1,8 +1,19 @@
 import {Component} from "react";
 import './Header.css';
 import 'bootstrap/dist/css/bootstrap-grid.rtl.css'
+import githubLogo from '../../resources/github-mark-white.svg'
+import linkedinLogo from '../../resources/linkedin.svg'
 
 class Header extends Component {
+
+    openGitWindow = () => {
+        window.open('https://github.com/AJMazur', '_blank')
+    }
+
+    openLinkedinWindow = () => {
+        window.open('https://www.linkedin.com/in/adam-mazur-49ab74195/', '_blank')
+    }
+
     render() {
         return (
             <div className="header-wrapper container-fluid col-10 offset-1">
@@ -10,12 +21,18 @@ class Header extends Component {
                     Header Title is here
                 </div>
                 <div className="header-navigation row-cols-3 ">
-                    <div className="header-navigation-button">
-                        item 1
-                    </div>
-                    <div className="header-navigation-button middle-button">
-                        item 2
-                    </div>
+                    <button onClick={this.openGitWindow} className="header-navigation-button">
+                        <img src={githubLogo} alt='github-mark' className='github-logo'></img>
+                        <div className='button-label'>
+                            item 1
+                        </div>
+                    </button>
+                    <button onClick={this.openLinkedinWindow} className="header-navigation-button middle-button">
+                        <img src={linkedinLogo} alt='linkedin' className='linkedin-logo'></img>
+                        <div className='button-label'>
+                            item 2
+                        </div>
+                    </button>
                     <div className="header-navigation-button">
                         item 3
                     </div>
