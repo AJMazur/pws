286440df62aa371bb3e83db46e7601d8ca66de8d
Adam <mazurekdab65@gmail.com>
2023-10-22 13:30:29 +0200

    Further implementation of email service

--- src/main/java/pws/common/data/EmailProperties.java
+++ src/main/java/pws/common/data/EmailProperties.java
@@ -2,8 +2,10 @@ package pws.common.data;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
