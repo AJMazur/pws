b13470b16542b531b0a9b69d2a8abf3bd9a6755a
Adam <mazurekdab65@gmail.com>
2023-11-07 21:33:27 +0100

    Styling the footer and changing the background color of header

--- pws-frontend/src/components/footer/Footer.jsx
+++ pws-frontend/src/components/footer/Footer.jsx
@@ -4,15 +4,25 @@ import './Footer.css'
 
 const Footer = () => {
 
-
     return (
         <div className="footer">
-            <label htmlFor="title">Subject: </label>
-            <input type="text" id="title" name="title"/>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email"/>
-            <label htmlFor="body">Body:</label>
-            <input type="text" id="body" name="body"/>
+            <div className="offset-1 col-10 container-fluid footer-title">
+                footer title
+            </div>
+            <div className="offset-2 col-8 container-fluid">
+                <div className="email-field-wrapper">
+                    <label className="field-label" htmlFor="email">Email:</label>
+                    <input className="email-field" type="email" id="email" name="email"/>
+                </div>
+                <div className="subject-field-wrapper">
+                    <label className="field-label" htmlFor="title">Subject: </label>
+                    <input className="subject-field" type="text" id="title" name="title"/>
+                </div>
+                <div className="body-field-wrapper">
+                    <label className="field-label" htmlFor="body">Body:</label>
+                    <textarea className="body-field" id="body" name="body"/>
+                </div>
+            </div>
         </div>
     )
 }
